2c9462febdb9a5a6b486b5aeefa37e2445abecb4
Dmitriy <apelsinka.channel@gmail.com>
2018-06-09 14:42:54 +0400

    Update Program.cs

--- AbstractPizzeriaView/Program.cs
+++ AbstractPizzeriaView/Program.cs
@@ -1,5 +1,5 @@
-﻿using AbstractPizzeriaService.ImplementationsList;
-using AbstractPizzeriaService.Interfaces;
+﻿using AbstractPizzeriaService.Interfaces;
+using AbstractPizzeriaService.ImplementationsList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
