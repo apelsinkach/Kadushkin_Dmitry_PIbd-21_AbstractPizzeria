82b0f613649941bb50ce19a2143a6b1b80e07230
Dmitry <apelsinka.channel@gmail.com>
2018-06-09 09:31:10 +0400

    Lab12

---
+++ AbstractPizzeriaView/ResourcesLoadForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AbstractPizzeriaView
+{
+    partial class ResourcesLoadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnResource = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnIngridient = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonSaveToExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnResource,
+            this.ColumnIngridient,
+            this.ColumnCount});
+            this.dataGridView.Location = new System.Drawing.Point(13, 48);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(555, 477);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ColumnResource
+            // 
+            this.ColumnResource.HeaderText = "Склад";
+            this.ColumnResource.Name = "ColumnResource";
+            // 
+            // ColumnIngridient
+            // 
+            this.ColumnIngridient.HeaderText = "Компонент";
+            this.ColumnIngridient.Name = "ColumnIngridient";
+            // 
+            // ColumnCount
+            // 
+            this.ColumnCount.HeaderText = "Количество";
+            this.ColumnCount.Name = "ColumnCount";
+            // 
+            // buttonSaveToExcel
+            // 
+            this.buttonSaveToExcel.Location = new System.Drawing.Point(178, 12);
+            this.buttonSaveToExcel.Name = "buttonSaveToExcel";
+            this.buttonSaveToExcel.Size = new System.Drawing.Size(230, 23);
+            this.buttonSaveToExcel.TabIndex = 1;
+            this.buttonSaveToExcel.Text = "Сохранить в Excel";
+            this.buttonSaveToExcel.UseVisualStyleBackColor = true;
+            this.buttonSaveToExcel.Click += new System.EventHandler(this.buttonSaveToExcel_Click);
+            // 
+            // ResourcesLoadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 537);
+            this.Controls.Add(this.buttonSaveToExcel);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "ResourcesLoadForm";
+            this.Text = "Загрузка складов";
+            this.Load += new System.EventHandler(this.ResourcesLoadForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnResource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIngridient;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCount;
+        private System.Windows.Forms.Button buttonSaveToExcel;
+    }
+}
