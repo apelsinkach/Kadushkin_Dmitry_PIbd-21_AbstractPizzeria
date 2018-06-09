fecb55bde29c2f67c658f845bd9fc298e3a41ddf
Dmitry <apelsinka.channel@gmail.com>
2018-06-09 15:30:04 +0400

    Lab13

---
+++ AbstractPizzeriaRestApi/Controllers/IngridientController.cs
@@ -0,0 +1,61 @@
+﻿using AbstractPizzeriaService.BindingModels;
+using AbstractPizzeriaService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AbstractPizzeriaRestApi.Controllers
+{
+    public class IngridientController : ApiController
+    {
+        private readonly IIngridientService _service;
+
+        public IngridientController(IIngridientService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            var list = _service.GetList();
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var element = _service.GetElement(id);
+            if (element == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(element);
+        }
+
+        [HttpPost]
+        public void AddElement(IngridientBindingModel model)
+        {
+            _service.AddElement(model);
+        }
+
+        [HttpPost]
+        public void UpdElement(IngridientBindingModel model)
+        {
+            _service.UpdElement(model);
+        }
+
+        [HttpPost]
+        public void DelElement(IngridientBindingModel model)
+        {
+            _service.DelElement(model.Id);
+        }
+    }
+}
--- AbstractPizzeriaView/ResourcesLoadForm.cs
+++ AbstractPizzeriaView/ResourcesLoadForm.cs
@@ -1,5 +1,6 @@
 ﻿using AbstractPizzeriaService.BindingModels;
 using AbstractPizzeriaService.Interfaces;
+using AbstractPizzeriaService.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,71 +10,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Unity;
-using Unity.Attributes;
+
 
 namespace AbstractPizzeriaView
 {
     public partial class ResourcesLoadForm : Form
     {
-        [Dependency]
-        public new IUnityContainer Container { get; set; }
-
-        private readonly IStatementService service;
-
-        public ResourcesLoadForm(IStatementService service)
+        public ResourcesLoadForm()
         {
             InitializeComponent();
-            this.service = service;
         }
 
         private void ResourcesLoadForm_Load(object sender, EventArgs e)
         {
             try
             {
-                var dict = service.GetStocksLoad();
-                                if (dict != null)
-                                    {
+                var response = APIClient.GetRequest("api/Statement/GetStocksLoad");
+                if (response.Result.IsSuccessStatusCode)
+                {
                     dataGridView.Rows.Clear();
-                                        foreach (var elem in dict)
-                                            {
+                    foreach (var elem in APIClient.GetElement<List<ResourcesLoadViewModel>>(response))
+                    {
                         dataGridView.Rows.Add(new object[] { elem.ResourceName, "", "" });
-                                                foreach (var listElem in elem.Ingridients)
-                                                   {
+                        foreach (var listElem in elem.Ingridients)
+                        {
                             dataGridView.Rows.Add(new object[] { "", listElem.Item1, listElem.Item2 });
-                                                    }
+                        }
                         dataGridView.Rows.Add(new object[] { "Итого", "", elem.TotalCount });
                         dataGridView.Rows.Add(new object[] { });
-                                            }
-                                    }
-                            }
-                        catch (Exception ex)
+                    }
+                }
+                else
+                {
+                    throw new Exception(APIClient.GetError(response));
+                }
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            }
             }
+        }
 
         private void buttonSaveToExcel_Click(object sender, EventArgs e)
         {
             SaveFileDialog sfd = new SaveFileDialog
             {
                 Filter = "xls|*.xls|xlsx|*.xlsx"
-                            };
-                        if (sfd.ShowDialog() == DialogResult.OK)
-                            {
-                                try
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
                 {
-                    service.SaveStocksLoad(new StatementBindingModel
-                                        {
+                    var response = APIClient.PostRequest("api/Statement/SaveStocksLoad", new StatementBindingModel
+                    {
                         FileName = sfd.FileName
-                                            });
-                    MessageBox.Show("Выполнено", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                    }
-                                catch (Exception ex)
+                    });
+                    if (response.Result.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Выполнено", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        throw new Exception(APIClient.GetError(response));
+                    }
+                }
+                catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                    }
-                            }
+                }
+            }
         }
     }
 }
