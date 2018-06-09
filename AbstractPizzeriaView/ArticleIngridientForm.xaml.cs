5d178bf7632574391914ef18232329672ba840a0
Dmitry <apelsinka.channel@gmail.com>
2018-06-09 15:50:54 +0400

    Lab9-10WPF

---
+++ AbstractPizzeriaView/ArticleIngridientForm.xaml.cs
@@ -0,0 +1,109 @@
+﻿using AbstractPizzeriaService.Interfaces;
+using AbstractPizzeriaService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractPizzeriaView
+{
+    /// <summary>
+    /// Логика взаимодействия для ArticleIngridientForm.xaml
+    /// </summary>
+    public partial class ArticleIngridientForm : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public ArticleIngridientViewModel Model { set { model = value; } get { return model; } }
+
+        private readonly IIngridientService service;
+
+        private ArticleIngridientViewModel model;
+
+        public ArticleIngridientForm(IIngridientService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCount.Text))
+            {
+                MessageBox.Show("Заполните поле Количество", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (comboBoxIngridient.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите компонент", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                if (model == null)
+                {
+                    model = new ArticleIngridientViewModel
+                    {
+                        IngridientId = Convert.ToInt32(comboBoxIngridient.SelectedValue),
+                        IngridientName = comboBoxIngridient.Text,
+                        Count = Convert.ToInt32(textBoxCount.Text)
+                    };
+                }
+                else
+                {
+                    model.Count = Convert.ToInt32(textBoxCount.Text);
+                }
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<IngridientViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    comboBoxIngridient.DisplayMemberPath = "IngridientName";
+                    comboBoxIngridient.SelectedValuePath = "Id";
+                    comboBoxIngridient.ItemsSource = list;
+                    comboBoxIngridient.SelectedItem = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (model != null)
+            {
+                comboBoxIngridient.IsEnabled = false;
+                comboBoxIngridient.SelectedValue = model.IngridientId;          
+                textBoxCount.Text = model.Count.ToString();
+            }
+        }
+    }
+}
---
+++ AbstractPizzeriaView/ResourceForm.xaml.cs
@@ -0,0 +1,107 @@
+﻿using AbstractPizzeriaService.BindingModels;
+using AbstractPizzeriaService.Interfaces;
+using AbstractPizzeriaService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractPizzeriaView
+{
+    /// <summary>
+    /// Логика взаимодействия для ResourceForm.xaml
+    /// </summary>
+    public partial class ResourceForm : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public int Id { set { id = value; } }
+
+        private readonly IResourceService service;
+
+        private int? id;
+
+        public ResourceForm(IResourceService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                if (id.HasValue)
+                {
+                    service.UpdElement(new ResourceBindingModel
+                    {
+                        Id = id.Value,
+                        ResourceName = textBoxName.Text
+                    });
+                }
+                else
+                {
+                    service.AddElement(new ResourceBindingModel
+                    {
+                        ResourceName = textBoxName.Text
+                    });
+                }
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    ResourceViewModel view = service.GetElement(id.Value);
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.ResourceName;
+                        dataGridView.ItemsSource = view.ResourceIngridients;
+                        dataGridView.Columns[0].Visibility = Visibility.Hidden;
+                        dataGridView.Columns[1].Visibility = Visibility.Hidden;
+                        dataGridView.Columns[2].Visibility = Visibility.Hidden;
+                        dataGridView.Columns[3].Width = DataGridLength.Auto;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
