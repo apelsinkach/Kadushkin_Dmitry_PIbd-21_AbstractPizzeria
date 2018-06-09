3bbe54ee2349612b2fcde57b87daae2874931236
Dmitry <apelsinka.channel@gmail.com>
2018-06-09 16:13:47 +0400

    Lab9WPF

---
+++ AbstractPizzeriaView/IngridientsForm.xaml.cs
@@ -0,0 +1,107 @@
+﻿using AbstractPizzeriaService.Interfaces;
+using AbstractPizzeriaService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+using Unity.Attributes;
+
+namespace AbstractPizzeriaView
+{
+    /// <summary>
+    /// Логика взаимодействия для IngridientsForm.xaml
+    /// </summary>
+    public partial class IngridientsForm : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        private readonly IIngridientService service;
+
+        public IngridientsForm(IIngridientService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                List<IngridientViewModel> list = service.GetList();
+                if (list != null)
+                {
+                    dataGridView.ItemsSource = list;
+                    dataGridView.Columns[0].Visibility = Visibility.Hidden;
+                    dataGridView.Columns[1].Width = DataGridLength.Auto;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<IngridientForm>();
+            if (form.ShowDialog() == true)
+            {
+                LoadData();
+            }
+        }
+
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedCells.Count == 1)
+            {
+                var form = Container.Resolve<IngridientForm>();
+                form.Id = Convert.ToInt32(dataGridView.SelectedValue);
+                if (form.ShowDialog() == true)
+                {
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedItems.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    int id = Convert.ToInt32(dataGridView.SelectedIndex + 1);
+                    try
+                    {
+                        service.DelElement(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
